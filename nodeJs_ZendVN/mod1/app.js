38f43051124ac86e156908fe51044fcd388238df
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-08-15 22:24:11 +0700

    output html file

--- nodeJs_ZendVN/mod1/app.js
+++ nodeJs_ZendVN/mod1/app.js
@@ -1,2 +1,46 @@
-console.log('Hello world!');
-console.log('Wellcome to my program.');
+// 
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+// const mod1 = require('./mod1.js');
+const modConfig = require('./mymodule/config.js');
+
+
+function onRequest(req, res)  {
+    // console.log( url.parse('http://localhost:8989/about'));
+
+    const path = url.parse(req.url).pathname;
+    console.log(path);
+    
+    if(path == '/about'){
+        fs.readFile('./view/about.html', (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.write("File not found!")
+            }
+            else{
+                res.write(data);
+            }
+              res.end();
+          });
+    }
+    else if(path == '/') {
+        fs.readFile('./view/study.html', (err, data) => {
+      if (err) {
+          res.writeHead(404);
+          res.write("File not found!")
+      }
+      else{
+          res.write(data);
+      }
+        res.end();
+    });
+    }
+
+    
+  
+  };
+
+
+http.createServer(onRequest).listen(modConfig.port);
---
+++ nodeJs_ZendVN/mod1/mod1.js
@@ -0,0 +1,12 @@
+
+
+const hostname = '127.0.0.1';
+const port = 3030;
+
+module.exports = {
+    show: function () {
+        console.log('Hello world 1.');
+    },
+    hostname: hostname,
+    port: port
+}
