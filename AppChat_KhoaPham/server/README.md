f0ed89b23defb36a520321b0405652ecc3143c35
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-10-27 18:47:52 +0700

    lắng nghe sự kiện goi den sẻrver

---
+++ AppChat_KhoaPham/server/README.md
@@ -0,0 +1,14 @@
+# Server
+
+## library
+    - express
+    - ejs
+    - socket.io
+    - nodemon
+
+##### npm add express ejs socket.io
+##### npm install socket.io
+##### npm add -D nodemon  || "start" : "nodemon server.js",
+
+## lắng nghe sự kiện nhận trên sẻrver
+
--- AppChat_KhoaPham/server/server.js
+++ AppChat_KhoaPham/server/server.js
@@ -5,10 +5,24 @@ app.use(express.static("./public"));
 app.set('view engine', 'ejs');
 app.set('views', './public');
 
-app.get('/', (req, res) => {
-  res.render('trangchu')
+const server = require('http').Server(app);
+const io = require("socket.io")(server);
+
+server.listen(8000, () => {
+  console.log(`Example app listening on port ${8000}`, ' - http://localhost:8000/')
+});
+
+// io xac nhan co nguoi ket noi
+io.on('connection', function (socket) {
+    console.log("co nguoi ket noi - " + socket.id);
+
+    socket.on('disconnect', function () {
+        console.log(socket.id + " - ngat ket noi");
+    })
 })
 
-app.listen(8000, () => {
-  console.log(`Example app listening on port ${8000}`)
-})
+app.get('/', (req, res) => {
+  res.render('trangchu')
+});
+
+
