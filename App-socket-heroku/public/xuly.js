f4caf2804152d2eb063a48011d1f636e153d8cc3
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-10-29 17:17:34 +0700

    demo heroku: finish server

--- App-socket-heroku/public/xuly.js
+++ App-socket-heroku/public/xuly.js
@@ -2,7 +2,23 @@
 let socket = io('http://localhost:8000')
 
 
+socket.on('server-send-listUser', (data) => {
+    $('#listUser').html("");
+    let i =1;
+    data.forEach(item => {
+        $('#listUser').append(`<div class="item"> id: ${i++} || <span id="itemName"> ${item.name}</span> || <span id="itemEmail"> ${item.email}</span> || <span id="itemPhone"> ${item.phone}</span></div>`)
+    });
+
+})
+
 $(document).ready(() => {
 
-    
+    $('#btnRegister').click(()=> {
+        let user = {
+            name : $('#txtName').val(),
+            email:  $('#txtEmail').val(),
+            phone:  $('#txtPhone').val(),
+        }
+        socket.emit('client-send-user-acc', user )
+    })
 })
--- App-socket-heroku/server.js
+++ App-socket-heroku/server.js
@@ -13,9 +13,29 @@ server.listen(port, () => {
     console.log(`Example app listening on port ${port}`, `- http://localhost:${port}/`)
 });
 
+//luu data User
+let arrUser = [];
+
 //
+io.on('connection', (socket) => {
+    console.log(`connect: ${socket.id}`);
 
+    //nghe sk gui acc user ve
+    socket.on('client-send-user-acc', (data) => {
+        
+        arrUser.push(
+            new User(data.name, data.email, data.phone)
+        )
 
+        io.sockets.emit('server-send-listUser', arrUser)
+    })
+})
+
+function User(name, email, phone) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+}
 
 // 
 app.get('/', (req, res) => {
